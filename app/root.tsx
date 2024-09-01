5839f509ac54ad22fa6e0edc32a4ac6f416f669d
kyrylo <kyrylo@buckhill.co.uk>
2024-09-01 20:38:14 +0200

    Update

--- app/root.tsx
+++ app/root.tsx
@@ -18,6 +18,18 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+
+        <script dangerouslySetInnerHTML={ { __html: `
+          (function(h:any,o:any,t:any,j:any,a:any,r:any){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:5119155,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        ` } }></script>
+
       </head>
       <body>
         {children}
