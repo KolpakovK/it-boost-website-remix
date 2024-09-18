7f6b1b4947049d49b2d94cfb0f824ccb5dcb1e3f
kyrylo <kyrylo@buckhill.co.uk>
2024-09-18 17:26:44 +0200

    Fixes

--- app/root.tsx
+++ app/root.tsx
@@ -1,9 +1,9 @@
 import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+    Links,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
 } from "@remix-run/react";
 import "./tailwind.css";
 
@@ -11,44 +11,65 @@ import '@fontsource/ibm-plex-sans';
 import '@fontsource/dela-gothic-one';
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <Meta />
+                <Links />
 
-        <script dangerouslySetInnerHTML={ { __html: `
-          (function(h,o,t,j,a,r){ 
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:5119155,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        ` } }></script>
+                <script dangerouslySetInnerHTML={{
+                    __html: `
+                        (function(h,o,t,j,a,r){ 
+                        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                        h._hjSettings={hjid:5119155,hjsv:6};
+                        a=o.getElementsByTagName('head')[0];
+                        r=o.createElement('script');r.async=1;
+                        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                        a.appendChild(r);
+                        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                ` }}></script>
 
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-5VKNP98KB4"></script>
-        <script dangerouslySetInnerHTML={ { __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-5VKNP98KB4"></script>
+                <script dangerouslySetInnerHTML={{
+                    __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
 
-          gtag('config', 'G-5VKNP98KB4');
-        ` } }></script>
+                        gtag('config', 'G-5VKNP98KB4');
+                ` }}></script>
 
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+
+                <script dangerouslySetInnerHTML={{
+                    __html: `
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '743338941257039');
+                    fbq('track', 'PageView');
+                ` }}></script>
+
+                <noscript><img height="1" width="1" style="display:none"
+                    src="https://www.facebook.com/tr?id=743338941257039&ev=PageView&noscript=1"
+                /></noscript>
+
+            </head>
+            <body>
+                {children}
+                <ScrollRestoration />
+                <Scripts />
+            </body>
+        </html>
+    );
 }
 
 export default function App() {
-  return <Outlet />;
+    return <Outlet />;
 }
